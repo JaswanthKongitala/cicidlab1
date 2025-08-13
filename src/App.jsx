7ef277a360ded48f104129f69c170d730c92e273
Your Name <you@example.com>
2025-08-13 10:26:40 +0530

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/cicdlab1">
+    <Router basename="/cicidlab1">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
